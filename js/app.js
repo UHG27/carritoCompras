185e0bb56996139a603fe789d126948804e7480c
Uriel Hernandez <urielHernandezgarcia1@gmail.com>
2022-09-27 13:05:53 -0500

    Finalizando

---
+++ js/app.js
@@ -0,0 +1,19 @@
+//Variables
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const listaCursos = document.querySelector('#lista-cursos');
+const vaciarCarrito = document.querySelector('#vaciar-carrito');
+
+cargarEventListener();
+function cargarEventListener(){
+    //
+    listaCursos.addEventListener('click', agregarCurso);
+}
+
+//Funciones
+
+function agregarCurso(e){
+    if(e.target.classList.contains('agregar-carrito')){
+console.log("Presionaste el boton");
+    }
+}
